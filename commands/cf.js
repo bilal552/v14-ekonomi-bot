f8749c7551c96030bf00acddc17362aedaa22aaa
AOPRAX <60624605+AOPRAX@users.noreply.github.com>
2023-07-16 20:43:24 +0300

    Update cf.js

--- commands/cf.js
+++ commands/cf.js
@@ -1,29 +1,27 @@
 const { EmbedBuilder } = require("discord.js");
-const slenzydb = require("croxydb")
+const slenzydb = require("croxydb");
+
 exports.run = async (client, message, args) => {
+    const money = slenzydb.fetch(`para_${message.author.id}`);
+    if (!money) return message.reply("Paranız yok.");
     
-    const money = slenzydb.fetch(`para_${message.author.id}`)
-    if (!money) return message.reply("Paran yok")
-    const moneyPush = args[0]
-    if (!moneyPush) return message.reply("Lütfen oynamak istediğin miktarı gir!")
-    if (money < moneyPush) return message.reply("yeterli paran yok!")
-
-    var Money = moneyPush
-    if(!moneyPush) Money = 1
+    const moneyPush = parseInt(args[0]);
+    if (isNaN(moneyPush) || moneyPush <= 0) return message.reply("Lütfen geçerli bir miktar girin!");
     
-    const mapping = ["true","false"]
-    const randomMapping = mapping[Math.floor(Math.random() * mapping.length)]
+    if (money < moneyPush) return message.reply("Yeterli paranız yok!");
     
-    if(randomMapping === "true") {
-        message.channel.send("**Kazandınız tebrikler!**")
-        slenzydb.add(`para_${message.author.id}`, moneyPush*2)
-    } 
-    if(randomMapping === "false") {
-            message.channel.send("**Kaybettiniz!**")
-            slenzydb.add(`para_${message.author.id}`, -moneyPush)
+    const mapping = ["true", "false"];
+    const randomMapping = mapping[Math.floor(Math.random() * mapping.length)];
+    
+    if (randomMapping === "true") {
+        message.channel.send("**Kazandınız, tebrikler!**");
+        slenzydb.add(`para_${message.author.id}`, moneyPush * 2);
+    } else {
+        message.channel.send("**Kaybettiniz!**");
+        slenzydb.add(`para_${message.author.id}`, -moneyPush);
     }
-
 };
+
 exports.conf = {
   aliases: []
 };
